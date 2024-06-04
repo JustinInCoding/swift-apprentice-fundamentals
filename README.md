1cdbc6b70e427821a799b1fe2ec8b54bdc24908c
JustinSheng <justin.wang4@icloud.com>
2024-06-04 19:23:49 +0800

    [init] playground

---
+++ README.md
@@ -0,0 +1,17 @@
+# Swift Apprentice
+[![en](https://img.shields.io/badge/lang-en-red.svg)](https://github.com/JustinInCoding/Swift-Apprentice/blob/main/README.md)
+[![zh-cn](https://img.shields.io/badge/lang-zh--cn-blue.svg)](https://github.com/JustinInCoding/Swift-Apprentice/blob/main/README.zh-cn.md)
+
+> This project is for me to learn from kodeco's iOS book Swift Apprentice. Here I try to grasp the knowledge of Swift Foundamental by recording the steps of learning the gramma.
+
+
+## Setup
+Xcode - 15.4 (the version I created the playground)
+
+## Tips and Learned
+
+
+## Acknowledgements
+Thanks for the kodeco's team providing such a great book
+- Original Repository of Kodeco [_here_](https://github.com/kodecocodes/saf-materials/tree/editions/1.0).
+
---
+++ swift-apprentice.playground/Pages/1. Expressions, Variables & Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+/// Copyright (c) 2024
+/// Justin
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+//: ## How a Computer Works
+//: The CPU stores the numbers it acts upon in small memory units called registers
+//: The CPU can read numbers into registers from the computer’s main memory, known as Random Access Memory (RAM).
+//: It can also write the number stored in a register back into RAM.
+
+//: Each time the CPU makes an addition, a subtraction, a read from RAM or a write to RAM, it’s executing a single instruction
+//: a compiler, which converts the code into those small machine instructions the CPU knows how to execute.
+
+//: ## Representing Numbers
+//: each character within a block of text is represented by a number.
+//: An image consists of thousands or millions of picture elements called pixels, where each pixel is a solid color. Each of these solid color pixels is usually represented by three numbers: one for the amount of red, one for the amount of green and one for the amount of blue.
+
+//: each digit of a binary number is a bit.
+//: Eight bits make up a byte.
+//: Four bits is called a nibble
+//: Each register, for example, is usually 32 or 64 bits in length, which is why we speak of 32-bit and 64-bit CPUs.
+//: hexadecimal or hex is base 16.
+
+//: ## How Code Works
+
+
+import Foundation
+
+var greeting = "Hello, playground"
+
+//: [Next](@next)
